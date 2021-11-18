29440702436c9a6f07f2c179133a7993364a2ecf
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-11-19 01:00:41 +0300

    Add files via upload

---
+++ solar_model.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+# license: GPLv3
+
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+    Параметры:
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    body.Fx = body.Fy = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        Rx = obj.x - body.x
+        Ry = obj.y - body.y
+        R = (Rx**2 + Ry**2)**0.5
+        R2 = (Rx**2 + Ry**2)
+        R = max(R, body.R)  # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        Fi = gravitational_constant * body.m * obj.m /R2
+        body.Fx += Fi * Rx / R
+        body.Fy += Fi * Ry / R
+
+
+def move_space_object(body, dt):
+    """Перемещает тело в соответствии с действующей на него силой.
+    Параметры:
+    **body** — тело, которое нужно переместить.
+    """
+
+    ax = body.Fx/body.m
+    body.Vx += ax * dt
+    body.x += body.Vx * dt
+    ay = body.Fy/body.m
+    body.Vy += ay*dt
+    body.y += body.Vy * dt
+
+
+def recalculate_space_objects_positions(space_objects, dt):
+    """Пересчитывает координаты объектов.
+    Параметры:
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **dt** — шаг по времени
+    """
+    for body in space_objects:
+        calculate_force(body, space_objects)
+    for body in space_objects:
+        move_space_object(body, dt)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
